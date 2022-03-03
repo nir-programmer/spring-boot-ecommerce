0079b08af037ba0e85c7b468e9fa5438c13c9d46
nir-programmer <niritzhak10@gmail.com>
2022-03-03 20:34:25 +0200

    Initial Commit

---
+++ src/main/java/org/nir/ecommerce/entity/Product.java
@@ -0,0 +1,54 @@
+package org.nir.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp //Hibernate will automatically create the timestamp
+    private Date dateCreated;
+
+    @UpdateTimestamp//Hibernate will update this automatically
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+
+
+
+}
---
+++ src/test/java/org/nir/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nir.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
